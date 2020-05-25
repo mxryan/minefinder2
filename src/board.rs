8596da2dc206a9eea9e5819a7817fc475b9b0062
Michael Ryan <mxryan@live.com>
2020-05-25 09:52:06 -0400

    basic rendering and some board logic

--- src/board.rs
+++ src/board.rs
@@ -1,16 +1,23 @@
-use rand::prelude::*;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
 
 #[derive(Debug)]
 pub struct Board {
     pub mines: i32,
-    pub rows: i32,
-    pub columns: i32,
+    pub rows: usize,
+    pub columns: usize,
     pub flags_placed: i32,
     pub cells: Vec<Cell>,
 }
 
 #[derive(Debug)]
-enum TileState {
+pub enum TileState {
     Hidden,
     Revealed,
     Flagged,
@@ -18,14 +25,14 @@ enum TileState {
 
 #[derive(Debug)]
 pub struct Cell {
-    neighboring_mines: i32,
-    state: TileState,
-    has_mine: bool,
+    pub neighboring_mines: i32,
+    pub state: TileState,
+    pub has_mine: bool,
 }
 
 impl Board {
-    pub fn new(mines: i32, rows: i32, columns: i32) -> Board {
-        let num_cells = (rows * columns) as usize;
+    pub fn new(mines: i32, rows: usize, columns: usize) -> Board {
+        let num_cells = (rows * columns);
         let mut cells: Vec<Cell> = Vec::with_capacity(num_cells);
         for _ in 0..num_cells {
             cells.push(Cell {
@@ -34,28 +41,26 @@ impl Board {
                 has_mine: false,
             });
         }
-
-        Board { mines, rows, columns, flags_placed: 0, cells: cells, }
+        Board { mines, rows, columns, flags_placed: 0, cells }
     }
 
     /// TODO: make sure the initial square clicked doesn't have a mine
     pub fn place_mines(&mut self) {
         let mut mines_to_place = self.mines;
-        let num_tiles = self.rows * self.columns;
-        let threshold = self.mines as f64 / num_tiles  as f64;
+        let num_tiles = (self.rows * self.columns) as usize;
+        let threshold = self.mines as f64 / num_tiles as f64;
+        let mut loop_count = 0;
+
         loop {
+            loop_count += 1;
             for i in 0..num_tiles {
                 if mines_to_place < 1 {
                     break;
                 }
-
-                if self.cells[i as usize].has_mine {
+                if self.cells[i].has_mine {
                     continue;
                 }
-
-                let mut rng = rand::thread_rng();
-                let rand: f64 = rng.gen();
-
+                let rand = get_random_f64();
                 if rand <= threshold {
                     self.cells[i as usize].has_mine = true;
                     mines_to_place -= 1;
@@ -66,9 +71,66 @@ impl Board {
                 break;
             }
         }
+        log(&format!("number of iterations required was {}", loop_count));
     }
 
-    pub fn get_total_number_of_cells(&self) -> i32 {
+    pub fn set_cells_num_bomb_neighbors(&mut self) {
+        for i in 0..self.get_total_number_of_cells() {
+            if self.cells[i].has_mine {
+                continue;
+            }
+            let mut cell = &mut self.cells[i];
+        }
+    }
+
+    pub fn count_neighboring_bombs(&self, i: usize) -> i32 {
+        let mut count = 0;
+        let (x, y) = self.index_to_coords(i);
+        for offset_x in -1i32..=1i32 {
+            let x_to_check = x as i32 + offset_x;
+            if x_to_check < 0 {
+                continue;
+            }
+            for offset_y in -1i32..=1i32 {
+                let y_to_check = y as i32 + offset_y;
+                if y_to_check < 0 || (x_to_check == 0 && y_to_check == 0) {
+                    continue;
+                }
+
+                let index_to_check = self.coords_to_index(x_to_check as usize,
+                                                          y_to_check as usize);
+                if self.cells[index_to_check].has_mine {
+                    count += 1;
+                }
+            }
+        }
+        count
+    }
+
+    pub fn coords_to_index(&self, x: usize, y: usize) -> usize {
+        (y * self.columns + x) as usize
+    }
+
+    pub fn index_to_coords(&self, i: usize) -> (usize, usize) {
+        (i % self.columns, i / self.columns)
+    }
+
+    pub fn get_total_number_of_cells(&self) -> usize {
         return self.rows * self.columns;
     }
+}
+
+fn get_random_buf() -> Result<[u8; 4], getrandom::Error> {
+    let mut buf = [0u8; 4];
+    getrandom::getrandom(&mut buf)?;
+    Ok(buf)
+}
+
+fn get_random_f64() -> f64 {
+    let four_random_bytes = get_random_buf().unwrap();
+    let mut bytes_as_u32 = four_random_bytes[0] as u32;
+    bytes_as_u32 += (four_random_bytes[1] as u32) << 8;
+    bytes_as_u32 += (four_random_bytes[2] as u32) << 16;
+    bytes_as_u32 += (four_random_bytes[3] as u32) << 24;
+    bytes_as_u32 as f64 / u32::MAX as f64
 }
--- src/lib.rs
+++ src/lib.rs
@@ -15,16 +15,15 @@ extern "C" {
 }
 
 
-// todo: these should live on the board
-const GRID_WIDTH: i32 = 16;
-const GRID_HEIGHT: i32 = 8;
-// todo: move this to a 'render_info' struct?
+const GRID_WIDTH: usize = 16;
+const GRID_HEIGHT: usize = 16;
 const PIXELS_PER_SQUARE_SIDE: f64 = 20.0;
 
 
 #[wasm_bindgen(start)]
 pub fn start() {
-    log("hi");
+    log("running");
+    log(&format!("yo: 5 % 2 = {} and -5 % 2 = {}", 5 % 2, -5 % 2));
     console_error_panic_hook::set_once();
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
@@ -43,22 +42,37 @@ pub fn start() {
     let mut board = Board::new(40, GRID_WIDTH, GRID_HEIGHT);
     render_grid(&context, &board);
 
-
-    let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+    let mut game_running = false;
+    let click_handler = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+        event.prevent_default();
         let x = event.offset_x();
         let y = event.offset_y();
         log(&format!("x: {}, y: {}", x, y));
-        log(&format!("{:?}", &board));
+        log(&format!("{:?}", event));
+        if !game_running {
+            board.place_mines();
+            game_running = true;
+            log("mines placed");
+        } else {
+            // update game state
+            log("game is running - updateBoardState()");
+        }
     }) as Box<dyn FnMut(_)>);
 
-    let res = canvas.add_event_listener_with_callback(
-        // todo: wtf is going on here.. as_ref.unchecked_ref()??
-        "mousedown", closure.as_ref().unchecked_ref(),
-    ).unwrap();
+    let context_menu_cb = Closure::wrap(
+        Box::new(move |event: web_sys::MouseEvent| {
+            event.prevent_default();
+        }) as Box<dyn FnMut(_)>);
 
-    log(&format!("{:?}", res));
-    log(&format!("By the way 5 / 2 = {}", 5i32 / 2i32));
-    closure.forget();
+    canvas.add_event_listener_with_callback(
+        "mousedown", click_handler.as_ref().unchecked_ref(),
+    ).expect("Failed to add the mousedown event listener");
+    click_handler.forget();
+
+    canvas.add_event_listener_with_callback(
+        "contextmenu", context_menu_cb.as_ref().unchecked_ref(),
+    ).expect("Failed to add the contextmenu event listener.");
+    context_menu_cb.forget();
 }
 
 fn render_grid(context: &web_sys::CanvasRenderingContext2d, board: &Board) {
@@ -66,10 +80,22 @@ fn render_grid(context: &web_sys::CanvasRenderingContext2d, board: &Board) {
     for i in 0..num_cells {
         let x = i % GRID_WIDTH;
         let y = i / GRID_WIDTH;
+        let mut fill_style: JsValue;
+        if board.cells[i as usize].has_mine {
+            fill_style = JsValue::from_str("rgba(255,0,255,1)");
+        } else {
+            fill_style = JsValue::from_str("rgba(125,50,255,1)");
+        }
+        context.set_fill_style(&fill_style);
+        context.fill_rect(x as f64 * PIXELS_PER_SQUARE_SIDE,
+                          y as f64 * PIXELS_PER_SQUARE_SIDE,
+                          PIXELS_PER_SQUARE_SIDE,
+                          PIXELS_PER_SQUARE_SIDE,
+        );
         context.stroke_rect(x as f64 * PIXELS_PER_SQUARE_SIDE,
                             y as f64 * PIXELS_PER_SQUARE_SIDE,
                             PIXELS_PER_SQUARE_SIDE,
-                            PIXELS_PER_SQUARE_SIDE
+                            PIXELS_PER_SQUARE_SIDE,
         );
     }
 }
