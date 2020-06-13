ba32ea5a29077563eb127c8eb27dc2c30c5031b2
Michael Ryan <mxryan@live.com>
2020-06-13 09:54:22 -0400

    small refactor

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ extern "C" {
 
 const GRID_WIDTH: usize = 16;
 const GRID_HEIGHT: usize = 16;
-const CELL_LEN: f64 = 20.0;
+const LEN: f64 = 20.0;
 const RGBA_MAGENTA: &str = "rgba(255,0,255,1)";
 const RGBA_CYAN: &str = "rgba(0,255,255,1)";
 const RGBA_WHITE: &str = "rgba(255,255,255,1)";
@@ -55,8 +55,12 @@ pub fn start() {
 
     render_grid(&context, &board);
 
-    let cloz = move |event: web_sys::MouseEvent| {
-        let (x, y) = convert_coords(event.offset_x(), event.offset_y());
+    let handle_click = move |event: web_sys::MouseEvent| {
+        let (x, y) = convert_coords_click_event_to_board(
+            event.offset_x(),
+            event.offset_y(),
+        );
+
         let click = match event.button() {
             0 => Click::Left,
             2 => Click::Right,
@@ -77,77 +81,80 @@ pub fn start() {
         render_grid(&context, &board);
     };
 
-    let click_handler = Closure::wrap(Box::new(cloz) as Box<dyn FnMut(_)>);
+    let click_handler = Closure::wrap(Box::new(handle_click) as Box<dyn FnMut(_)>);
 
     let context_menu_cb = Closure::wrap(
-        Box::new(|event: web_sys::MouseEvent| {
-            event.prevent_default();
-        }) as Box<dyn FnMut(_)>);
+        Box::new(
+            |event: web_sys::MouseEvent| { event.prevent_default(); }
+        ) as Box<dyn FnMut(_)>
+    );
 
-    canvas.add_event_listener_with_callback(
-        "mousedown", click_handler.as_ref().unchecked_ref(),
-    ).expect("Failed to add the mousedown event listener");
+    let click_handler_unchecked = click_handler.as_ref().unchecked_ref();
+    canvas
+        .add_event_listener_with_callback("mousedown", click_handler_unchecked)
+        .expect("Failed to add the mousedown event listener");
     click_handler.forget();
 
-    canvas.add_event_listener_with_callback(
-        "contextmenu", context_menu_cb.as_ref().unchecked_ref(),
-    ).expect("Failed to add the contextmenu event listener.");
+    let context_menu_cb_unchecked = context_menu_cb.as_ref().unchecked_ref();
+    canvas
+        .add_event_listener_with_callback("contextmenu", context_menu_cb_unchecked)
+        .expect("Failed to add the contextmenu event listener.");
     context_menu_cb.forget();
 }
 
 fn render_grid(context: &web_sys::CanvasRenderingContext2d, board: &Board) {
     let num_cells = board.get_total_number_of_cells();
+
     for i in 0..num_cells {
-        let x = (i % GRID_WIDTH) as f64;
-        let y = (i / GRID_WIDTH) as f64;
+        let x = (i % GRID_WIDTH) as i32;
+        let y = (i / GRID_WIDTH) as i32;
+        let (canv_x, canv_y) = convert_coords_board_to_canvas(x, y);
 
-        // todo: this only needs to be called first time the board is drawn
-        //  and instead of drawing each individual rect i could just draw a
-        //  bunch of straight lines
-        context.stroke_rect(x * CELL_LEN, y * CELL_LEN, CELL_LEN, CELL_LEN);
-
+        // todo: only draw once. can draw grid with lines instead of rects
+        context.stroke_rect(canv_x, canv_y, LEN, LEN);
 
         let font = "14px serif";
         context.set_font(font);
+
+        let font_x = canv_x + LEN / 2.5;
+        let font_y = canv_y + LEN / 1.5;
+
         match board.cells[i].state {
             CellState::Hidden => {
                 context.set_fill_style(&JsValue::from_str(RGBA_MAGENTA));
-                context.fill_rect(x * CELL_LEN, y * CELL_LEN, CELL_LEN, CELL_LEN);
+                context.fill_rect(canv_x, canv_y, LEN, LEN);
             }
 
             CellState::Flagged => {
                 context.set_fill_style(&JsValue::from_str(RGBA_MAGENTA));
-                context.fill_rect(x * CELL_LEN, y * CELL_LEN, CELL_LEN, CELL_LEN);
-                context.stroke_text("F", x * CELL_LEN + CELL_LEN / 2.5,
-                                    y * CELL_LEN + CELL_LEN / 1.5, );
+                context.fill_rect(canv_x, canv_y, LEN, LEN);
+                context.stroke_text("F", font_x, font_y);
             }
 
             CellState::Revealed => {
                 context.set_fill_style(&JsValue::from_str(RGBA_CYAN));
-                context.fill_rect(x * CELL_LEN, y * CELL_LEN, CELL_LEN, CELL_LEN);
+                context.fill_rect(canv_x, canv_y, LEN, LEN);
+
                 if board.cells[i].has_mine {
-                    context.stroke_text("X", x * CELL_LEN + CELL_LEN / 2.5,
-                                        y * CELL_LEN + CELL_LEN / 1.5
-                    ).expect("Failed context.stroke_text");
+                    context.stroke_text("X", font_x, font_y)
+                        .expect("Failed context.stroke_text");
                 } else {
-                    context.stroke_text(
-                        &format!("{}", board.cells[i].neighboring_mines),
-                        x * CELL_LEN + CELL_LEN / 2.5,
-                        y * CELL_LEN + CELL_LEN / 1.5,
-                    ).expect("Failed context.stroke_text");
+                    let text = &format!("{}", board.cells[i].neighboring_mines);
+                    context.stroke_text(text, font_x, font_y)
+                        .expect("Failed context.stroke_text");
                 }
             }
         }
     }
 }
 
-/// Translates the click event coordinates to the relative coordinates used by
-/// the game board.
-fn convert_coords(x: i32, y: i32) -> (i32, i32) {
-    (x / CELL_LEN as i32, y / CELL_LEN as i32)
+/// Converts offset_x and offset_y from click event to game board coordinates
+fn convert_coords_click_event_to_board(x: i32, y: i32) -> (i32, i32) {
+    (x / LEN as i32, y / LEN as i32)
 }
 
-// todo: these...
-// fn board_coords_from_canvas_coords
-// fn canvas_coards_from_board_coords
+/// Converts board coordinates to canvas coordinates (draw functions expect f64)
+fn convert_coords_board_to_canvas(x: i32, y: i32) -> (f64, f64) {
+    (x as f64 * LEN, y as f64 * LEN)
+}
 
